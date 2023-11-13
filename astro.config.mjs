31c7d39c9593438a5d676cb45dc82b48a5ff85bf
MicaAvigliano <micaela.avigliano@gmail.com>
2023-11-13 14:11:45 -0300

    more styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
-import astroI18next from "astro-i18next";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [astroI18next()],
-});
+export default defineConfig({});
