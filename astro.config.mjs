fa5b4e3ffa06e0a6a3a5ba11fb6a30c754da7e55
MicaAvigliano <micaela.avigliano@gmail.com>
2023-11-13 22:08:17 -0300

    add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
-
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     i18n: {
       defaultLocale: "es",
       locales: ["en", "es"],
-      routingStrategy: "prefix-always",
+      fallback: {
+        en: "es",
+      },
     },
   },
+  trailingSlash: "always",
 });
