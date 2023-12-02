17a43d42d382ac628c5d6a2623d4f871c658e110
MicaAvigliano <micaela.avigliano@gmail.com>
2023-12-02 18:14:58 -0300

    fix redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   experimental: {
     i18n: {
-      defaultLocale: "/",
+      defaultLocale: "es",
       locales: ["en", "es"],
       fallback: {
         en: "es",
       },
 
       routingStrategy: "prefix-always",
-      trailingSlash: "always",
     },
   },
 });
