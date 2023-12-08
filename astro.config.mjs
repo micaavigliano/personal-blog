6e8c95fa39d9ce4cd8e96a5a7550a71faeba5299
MicaAvigliano <micaela.avigliano@gmail.com>
2023-12-08 13:54:18 -0300

    upgrade astro to version 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    i18n: {
-      defaultLocale: "es",
-      locales: ["en", "es"],
-      fallback: {
-        en: "es",
-      },
-
-      routingStrategy: "prefix-always",
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: true,
     },
   },
 });
